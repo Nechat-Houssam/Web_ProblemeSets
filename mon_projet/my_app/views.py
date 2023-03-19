99a5607ede5b30a53e2d43bdc646ed65ec92358e
Nechat-Houssam <houssam.nechat@dauphine.eu>
2023-03-19 23:14:16 +0100

    entries highligth

--- mon_projet/my_app/views.py
+++ mon_projet/my_app/views.py
@@ -1,20 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 
 # Create your views here.
-
 
 entries = []
 
 def home(request):
-    
     context = {
         'entries': entries
     }
 
-    
+   
+    entry_id = request.GET.get('entry_id')
+    if entry_id:
+        context['entry_id'] = int(entry_id)
+
     return render(request, 'home.html', context)
+
 
 
 def add_entry(request):
@@ -23,5 +25,9 @@ def add_entry(request):
         name = request.POST.get('name')
         name = name.capitalize()
         entries.append({'name': name, 'content': content})
-
-    return render(request, 'add_entry.html')
+        entry_id = len(entries) - 1
+       
+        return HttpResponseRedirect('/my_app/?entry_id={}'.format(entry_id))
+    else:
+   
+        return render(request, 'add_entry.html')
