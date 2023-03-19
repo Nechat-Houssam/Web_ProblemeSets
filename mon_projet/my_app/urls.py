aeedd8fa26f5f24874b8fa596b2ecf068bade11f
Nechat-Houssam <houssam.nechat@dauphine.eu>
2023-03-19 23:20:33 +0100

    ajout des JSON

--- mon_projet/my_app/urls.py
+++ mon_projet/my_app/urls.py
@@ -1,9 +1,8 @@
-
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add_entry, name='add_entry'),
+    path('entry/<int:entry_id>/', views.entry, name='entry'),
 ]
--- mon_projet/my_app/views.py
+++ mon_projet/my_app/views.py
@@ -1,7 +1,6 @@
+import json
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
-
-# Create your views here.
+from django.http import HttpResponseRedirect, JsonResponse
 
 entries = []
 
@@ -10,24 +9,29 @@ def home(request):
         'entries': entries
     }
 
-   
+    # Check if an entry_id parameter is present in the GET parameters
     entry_id = request.GET.get('entry_id')
     if entry_id:
         context['entry_id'] = int(entry_id)
 
     return render(request, 'home.html', context)
 
-
-
 def add_entry(request):
     if request.method == 'POST':
         content = request.POST.get('content')
         name = request.POST.get('name')
         name = name.capitalize()
-        entries.append({'name': name, 'content': content})
-        entry_id = len(entries) - 1
-       
+        entry_id = len(entries) + 1
+        entries.append({'id': entry_id, 'name': name, 'content': content})
+        # Redirect to the homepage with an entry_id parameter
         return HttpResponseRedirect('/my_app/?entry_id={}'.format(entry_id))
-    else:
-   
-        return render(request, 'add_entry.html')
+    return render(request, 'add_entry.html')
+
+def entry(request, entry_id):
+    # Find the entry with the specified id
+    for entry in entries:
+        if entry['id'] == entry_id:
+            # Return the entry as JSON
+            return JsonResponse(entry)
+    # If the entry was not found, return a 404 error
+    return JsonResponse({'error': 'Entry not found.'}, status=404)
