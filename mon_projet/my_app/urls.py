f6e7add44139bb4eea8a94303f7d66a20c75ac4e
Nechat-Houssam <houssam.nechat@dauphine.eu>
2023-04-03 03:30:40 +0200

    Probleme set 5

--- mon_projet/my_app/urls.py
+++ mon_projet/my_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'my_app'
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add_entry, name='add_entry'),
--- mon_projet/my_app/views.py
+++ mon_projet/my_app/views.py
@@ -1,37 +1,40 @@
 import json
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect, JsonResponse
-
-entries = []
+from .models import Quote, Person
 
 def home(request):
+    quotes = Quote.objects.all()
     context = {
-        'entries': entries
+        'quotes': quotes
     }
-
-    # Check if an entry_id parameter is present in the GET parameters
-    entry_id = request.GET.get('entry_id')
-    if entry_id:
-        context['entry_id'] = int(entry_id)
-
     return render(request, 'home.html', context)
 
 def add_entry(request):
     if request.method == 'POST':
         content = request.POST.get('content')
-        name = request.POST.get('name')
-        name = name.capitalize()
-        entry_id = len(entries) + 1
-        entries.append({'id': entry_id, 'name': name, 'content': content})
-        # Redirect to the homepage with an entry_id parameter
-        return HttpResponseRedirect('/my_app/?entry_id={}'.format(entry_id))
-    return render(request, 'add_entry.html')
+        person_id = request.POST.get('person')
+        person = Person.objects.get(pk=person_id)
+        quote = Quote(content=content, person=person)
+        quote.save()
+        # redirection
+
+        return HttpResponseRedirect('/my_app/')
+    # Retrieve all persons to populate the dropdown list
+    persons = Person.objects.all()
+    context = {
+        'persons': persons
+    }
+    return render(request, 'add_entry.html', context)
 
 def entry(request, entry_id):
-    # Find the entry with the specified id
-    for entry in entries:
-        if entry['id'] == entry_id:
-            # Return the entry as JSON
-            return JsonResponse(entry)
-    # If the entry was not found, return a 404 error
-    return JsonResponse({'error': 'Entry not found.'}, status=404)
+    try:
+        quote = Quote.objects.get(pk=entry_id)
+        data = {
+            'id': quote.pk,
+            'name': quote.person.name,
+            'content': quote.content
+        }
+        return JsonResponse(data)
+    except Quote.DoesNotExist:
+        return JsonResponse({'error': 'enté non trouvée'}, status=404)
