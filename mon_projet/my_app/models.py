26cebea2f8086399024f9fbbb8bb9e8f62b63455
Nechat-Houssam <houssam.nechat@dauphine.eu>
2023-04-03 02:40:34 +0200

    models.py

--- mon_projet/my_app/models.py
+++ mon_projet/my_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Quote(models.Model):
+    content = models.TextField()
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
